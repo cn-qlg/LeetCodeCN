7f576fc95e4c280360f07a930de3e89ade7dfd79
liuguoqing <liuguoqing>
2020-11-16 14:10:40 +0800

    number 13

---
+++ 13.罗马数字转整数.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode.cn id=13 lang=python3
+#
+# [13] 罗马数字转整数
+#
+
+# @lc code=start
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        if not s:
+            return 0
+        roman_values = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+
+        result = 0
+        pre = 0
+        for c in s:
+            v = roman_values[c]
+            if pre < v:
+                result += v - pre * 2
+            else:
+                result += v
+            pre = v
+        return result
+
+
+# @lc code=end
+print(Solution().romanToInt("III"))
+print(Solution().romanToInt("IV"))
+print(Solution().romanToInt("IX"))
+print(Solution().romanToInt("LVIII"))
+print(Solution().romanToInt("MCMXCIV"))
