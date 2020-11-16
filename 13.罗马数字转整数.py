14477760c037a667de88b27ce4e7cdb13b384ca5
liuguoqing <liuguoqing>
2020-11-16 14:12:25 +0800

    number 13

--- 13.罗马数字转整数.py
+++ 13.罗马数字转整数.py
@@ -7,6 +7,10 @@
 # @lc code=start
 class Solution:
     def romanToInt(self, s: str) -> int:
+        """通常情况下，罗马数字中小的数字在大的数字的右边。
+        如果当前的值大于左边的数字，则直接减去左边值乘以2（因为之前加过一个了）
+        否则加上当前值
+        """
         if not s:
             return 0
         roman_values = {
