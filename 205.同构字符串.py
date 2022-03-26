2b755f0d40bea6e30cff0cd764b19237c4195640
cn_qlg <cn_qlg@163.com>
2022-03-26 15:01:01 +0800

    num 205

--- 205.同构字符串.py
+++ 205.同构字符串.py
@@ -6,27 +6,38 @@
 
 # @lc code=start
 
+# class Solution:
+#     def isIsomorphic(self, s: str, t: str) -> bool:
+#         """
+#         同时存下s->t和t->s的映射关系。
+#         """
+#         map1 = dict()
+#         map2 = dict()
+#         if len(s) != len(t):
+#             return False
+#         for i, c in enumerate(s):
+#             if t[i] in map1:
+#                 if map1[t[i]] != c:
+#                     return False
+#             else:
+#                 map1[t[i]] = c
+#             if c in map2:
+#                 if map2[c] != t[i]:
+#                     return False
+#             else:
+#                 map2[c] = t[i]
+#         return True
+
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        map1 = dict()
-        map2 = dict()
-        if len(s) != len(t):
-            return False
-        for i, c in enumerate(s):
-            if t[i] in map1:
-                if map1[t[i]] != c:
-                    return False
-            else:
-                map1[t[i]] = c
-            if c in map2:
-                if map2[c] != t[i]:
-                    return False
-            else:
-                map2[c] = t[i]
-        return True
+        """一个更pythonic的解法"""
+        return len(set(s)) == len(set(t)) == len(set(zip(s, t)))
 # @lc code=end
 
 
 if __name__ == "__main__":
     print(Solution().isIsomorphic("badc", "baba") == False)
     print(Solution().isIsomorphic("foo", "bar") == False)
+    print(list(zip("foo", "bar")))
+    print(set(zip("foo", "bar")))
+    print(("a", "b") == ("b", "a"))
