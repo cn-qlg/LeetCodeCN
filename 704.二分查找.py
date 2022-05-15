d0c45cb6d2d79ec12b9f9b42fd749599d97fb3e9
cn_qlg <cn_qlg@163.com>
2022-05-15 20:43:47 +0800

    num 704

---
+++ 704.二分查找.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=704 lang=python3
+#
+# [704] 二分查找
+#
+from typing import List
+# @lc code=start
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        i, j = 0, len(nums)-1
+        while i <= j:
+            h = (i+j)//2
+            if nums[h] == target:
+                return h
+            elif nums[h] > target:
+                j = h - 1
+            else:
+                i = h + 1
+        return -1
+
+
+# @lc code=end
+print(Solution().search([-1, 0, 3, 5, 9, 12], 9))
+print(Solution().search([-1, 0, 3, 5, 9, 12], -2))
