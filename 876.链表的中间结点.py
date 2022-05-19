9b04103070372993141c98bb71bac53806dcae5a
cn_qlg <cn_qlg@163.com>
2022-05-19 19:57:33 +0800

    update 876

--- 876.链表的中间结点.py
+++ 876.链表的中间结点.py
@@ -3,6 +3,11 @@
 #
 # [876] 链表的中间结点
 #
+""" 
+解题思路：
+1. 第一次遍历获取链表总长度，第二次遍历到一半即可直接返回
+2. 通过快慢指针，快指针一次遍历两格，慢指针一次走一格
+"""
 from typing import List
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -15,17 +20,24 @@ class ListNode:
 #         self.val = val
 #         self.next = next
 class Solution:
+    # def middleNode(self, head: ListNode) -> ListNode:
+    #     h = head
+    #     cnt = 0
+    #     while h is not None:
+    #         h = h.next 
+    #         cnt += 1
+    #     mid = cnt //2
+    #     p = head
+    #     while mid > 0:
+    #         p = p.next 
+    #         mid -= 1
+    #     return p
     def middleNode(self, head: ListNode) -> ListNode:
-        h = head
-        cnt = 0
-        while h is not None:
-            h = h.next 
-            cnt += 1
-        mid = cnt //2
         p = head
-        while mid > 0:
-            p = p.next 
-            mid -= 1
+        q = head
+        while q is not None and q.next is not None:
+            p = p.next
+            q = q.next.next
         return p
 
 # @lc code=end
