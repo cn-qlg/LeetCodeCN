6fc4ae0b49563f5a0dfb6bb252858c1983ac55f2
guoqing <guoqing@blidz.com>
2024-04-28 10:27:26 +0800

    125

--- Python/125.验证回文串.py
+++ Python/125.验证回文串.py
@@ -4,8 +4,29 @@
 # [125] 验证回文串
 #
 
-# @lc code=start
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        pass
-# @lc code=end
+        i = 0
+        j = len(s) -1
+        while True:
+            if i >= j:
+                break
+
+            if not s[i].isalnum():
+                i += 1
+                continue
+            if not s[j].isalnum():
+                j -= 1
+                continue
+            if s[i].upper() != s[j].upper():
+                return False
+            i += 1
+            j -= 1
+        return True
+    
+if __name__ == "__main__":
+    print(Solution().isPalindrome("A man, a plan, a canal: Panama"))
+    print(Solution().isPalindrome("race a car"))
+    print(Solution().isPalindrome(" "))
+    print(Solution().isPalindrome("A"))
+
